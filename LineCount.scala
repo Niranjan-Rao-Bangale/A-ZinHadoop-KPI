41431abf38639dd01a077a5c33b9d046b9bf35fb
Niranjan Rao <niranjan.rao@kpiparterns.com>
2016-08-25 19:49:54 +0530

    Inital Commit
    Adding all the files to A-ZinHadoop Repository

---
+++ LineCount.scala
@@ -0,0 +1,18 @@
+
+package a_z.in.hadoop.Maven_Example1
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+object Line_Count{
+  def main(args: Array[String]) {
+    System.setProperty("hadoop.home.dir", "D:\\Spark\\")
+
+    val logFile = "D:\\Softwares\\Spark_Binary\\spark-1.3.1-bin-hadoop2.4\\README.md" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))  
+  }
+}
